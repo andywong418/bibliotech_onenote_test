9ecdb5cc12b0c1df9cb8b1bd44bf469e22e252e1
andywong418 <wong22@hotmail.co.uk>
2015-07-23 16:49:23 +0100

    First commit

--- config.js
+++ config.js
@@ -1,6 +1,8 @@
 module.exports = {
     //Live Connect API information
-    clientId: '000000004011C945',
-    clientSecret: 'U5ofKaQiol75VbohV4hh-2x8XZpcJoPf ',
+    clientId: '000000004015CDBA',
+    clientSecret: 'cFkJ8QYl5K7CU9Z2oNDErEaGCbY2XL9C ',
     redirectUrl: 'http://onenoteapisamples.com:3000/callback'
+    
+
 };
